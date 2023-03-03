2314f5051e3d530b534e9427bd4181f3ebd2d1ed
Rushikesh Kothawade <97045515+RushikeshKothawade07@users.noreply.github.com>
2023-03-03 11:15:17 +0530

    Create triangle01.cpp

---
+++ pattern/triangle01.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+
+            if ((i + j) % 2 == 0)
+            {
+                cout << "1";
+            }
+            else
+            {
+                cout << "0";
+            }
+        }
+
+        cout << endl;
+    }
+}
+int main()
+{
+    int n = 4, m = 5;
+    solve(n);
+}
